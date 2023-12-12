03db6774830415d4fe36df4b67c170c1745c2dec
Luis Rondon <luisrondon2605@yahoo.com>
2023-12-12 12:10:58 +0100

    setting up backend again

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,29 @@
-from fastapi import FastAPI
+#uvicorn main:app
+#uvicorn main:app --reload
 
+#main imports
+from fastapi import FastAPI, File, UploadFile, HTTPException 
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai 
+
+#custom function imports
+
+
+
+#initiate app
 app = FastAPI()
+
+#CORS -origins
+
+origins =[
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:4174",
+    "http://localhost:3000",
+]
 
 
 @app.get("/")
