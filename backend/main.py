88fb276c51d1e58fd8e77b20e540f7f721f256ab
Luis Rondon <luisrondon2605@yahoo.com>
2023-12-12 12:37:03 +0100

    binding main imports

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@
 #main imports
 from fastapi import FastAPI, File, UploadFile, HTTPException 
 from fastapi.responses import StreamingResponse
+
 from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
 import openai 
@@ -24,6 +25,15 @@ origins =[
     "http://localhost:4174",
     "http://localhost:3000",
 ]
+#CORS-Middleware 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")
